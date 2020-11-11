81b3d19ee810003e31010d5674836642f78f81db
AppliNH <thomas.martindev@gmail.com>
2020-11-11 14:30:22 +0100

    drafting concept

---
+++ main.py
@@ -0,0 +1,16 @@
+# CLI PARTI
+
+def main():
+    print("hi CLI")
+
+    # dict_boi = function_id3_shit()
+
+
+    # "key1" ?
+    #  answer ==  valueOfKey1 ?
+    #  
+
+
+
+if __name__ == "__main__": 
+    main()
