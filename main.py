471527000bf0d42437f9759d11c65da051a90235
Víctor Cavero <victorgracia7@gmail.com>
2020-11-11 22:58:37 +0200

    Main funcionality done (Without the part of adding the new data in the .csv)

--- main.py
+++ main.py
@@ -1,28 +1,70 @@
-# CLI PART
-
-
 import ID3
+import pandas as pd
+from PyInquirer import prompt
 
 def main():
-    print("hi CLI")
 
-    fake_tree_dict = {'outlook': {'overcast': True,
-             'rainy': {'windy': {'FALSE': True, 'TRUE': False}},
-             'sunny': {'humidity': {'high': False, 'normal': True}}}}
+    fileName = 'data.csv'
+    answers = dict() # to store the answers of the user 
+    tree = ID3.generate_decision_tree(csv_filename=fileName) # Generate the ID3 Tree
 
-    tree = ID3.generate_decision_tree(csv_filename="data.csv")
+    df = pd.read_csv(fileName) # To know columns of the dataset
+    for x in df.columns : answers[x] = ''
 
-    print(tree)
+    right_prediction = go_through_tree_dict(tree,df,answers)
+    
+    if(not right_prediction):
+        # Ask non asked questions
+        for key,value in answers.items():
+            if (value == ''):
+                answer = ask_question(key,df) # With key we define the question we want to ask
+                answers[key] = answer
+    
+    print(answers) # Print the final obtained data
+                
 
-    # dict_boi = function_id3_shit()
+def ask_question(key,df):
+    # In this method we use the key to create the question and 
+    # the dataframe to know all the posibilities for each key 
+    possibilities = df.groupby(key).count().index.get_level_values(key)
 
+    question = {
+            'type': 'list',
+            'name': key,
+            'message': key,
+            'choices': possibilities
+        }
 
-    # "key1" ?
-    #  answer ==  valueOfKey1 ?
-    #  
+    answer_dict = prompt(question)
+    answer = answer_dict[key]
 
-def go_through_tree_dict(dict_boi: dict):
-    print('go_through_answer_dict')
+    return answer
 
-if __name__ == "__main__": 
+def go_through_tree_dict(tree,df,answers):
+    leaf = False
+    while(leaf == False):
+        if(type(tree) is dict):
+            key = list(tree.keys())[0]
+            tree = tree[key]
+            
+            answer = ask_question(key,df)
+            answers[key] = answer
+            tree = tree[answer]
+
+        else :
+            # when the first loop ends we have in 'tree' the final predicted value
+            predicted = tree
+            leaf = True
+
+            print('Predicted value for ' + df.columns[-1] + ' is ' + str(predicted))
+    
+            answer = ask_question(df.columns[-1], df)
+            answers[df.columns[-1]] = answer
+
+    if(answer.lower() in ['yes','true']): answer_value = True
+    else : answer_value = False 
+
+    return(answer_value == predicted)
+
+if __name__ == '__main__': 
     main()
