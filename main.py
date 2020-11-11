0fcbab67a2731d9a64fe86f653bb4596284404cf
AppliNH <thomas.martindev@gmail.com>
2020-11-11 14:32:29 +0100

    drafting the CLI

--- main.py
+++ main.py
@@ -3,6 +3,12 @@
 def main():
     print("hi CLI")
 
+    answer_dict = {'outlook': {'overcast': True,
+             'rainy': {'windy': {'FALSE': True, 'TRUE': False}},
+             'sunny': {'humidity': {'high': False, 'normal': True}}}}
+
+    
+
     # dict_boi = function_id3_shit()
 
 
@@ -10,7 +16,8 @@ def main():
     #  answer ==  valueOfKey1 ?
     #  
 
-
+def go_through_answer_dict(dict_boi: dict):
+    print('go_through_answer_dict')
 
 if __name__ == "__main__": 
     main()
