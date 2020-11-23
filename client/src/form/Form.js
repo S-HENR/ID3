b3dd3ddff2819ae675dbaaf46fc5e9a74876eb47
S-HENR <seraphin.henry@orange.fr>
2020-11-23 17:21:16 +0200

    Update : - Adaptative form which follows ID3 tree - No css - The application only handle good answer at the end

--- client/src/form/Form.js
+++ client/src/form/Form.js
@@ -1,33 +1,116 @@
 import React from 'react';
 import './Form.css';
+import { SURVEY } from '../services/surveyService';
 
-export default class Form extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: ''};
+export default class WholeForm extends React.Component {
   
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+  constructor(props) {
+    super(props)
+    this.survey = SURVEY
+    this.buildQuestion()
+  }
+  survey
+  currentQuestion
+  isFinalQuestion = false
+
+  state = { value:"" }
+
+  handleChange = (e) => {
+      this.setState({
+        value: e.target.value
+      })
+  }
+
+  handleIntermediateSubmit = (e) => {
+    e.preventDefault()
+    if(this.state.value === "")
+      return
+
+    this.survey = this.survey[this.state.value]
+
+    if(typeof(this.survey) == "string") {
+      this.isFinalQuestion = true
     }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
+    else {
+      this.buildQuestion()
     }
-  
-    handleSubmit(event) {
-      alert('Le nom a été soumis : ' + this.state.value);
-      event.preventDefault();
+      
+    
+    this.state.value = ""
+    console.log(this.isFinalQuestion)
+    this.forceUpdate();
+  };
+
+  handleFinalSubmit = (e) => {
+    e.preventDefault()
+    if(this.survey === this.state.value) {
+      alert('Thank you for your time !')
     }
-  
-    render() {
+    else {
+      this.askOtherQuestions()
+    }
+  }
+
+  buildQuestion() {
+    const [key, value] = Object.entries(this.survey)[0]
+
+    this.currentQuestion = {
+      question: key,
+      answers: Object.keys(value)
+    }
+    this.survey = this.survey[key]
+
+  }
+
+  askOtherQuestions() {
+    console.log("ok let's go")
+  }
+
+
+  render() {
+    if(!this.isFinalQuestion) {
       return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Nom :
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Envoyer" />
+        <form>
+          {this.currentQuestion.question}
+          {this.currentQuestion.answers.map(answer => 
+            <div>
+              <input 
+                id={answer}
+                type="radio" 
+                value={answer}
+                onChange={this.handleChange}
+                checked={this.state.value === answer}
+              />
+              <label for={answer}>{answer}</label>
+            </div>
+          )}
+          <button onClick={this.handleIntermediateSubmit}>Next</button>
+        </form>
+      );
+    }
+    else {
+      return (
+        <form>
+          Do you want to play ?
+          <input 
+            id="yes"
+            type="radio" 
+            value="yes"
+            onChange={this.handleChange}
+            checked={this.state.value === "yes"}
+          />
+          <label for="yes">Yes</label>
+          <input 
+            id="no"
+            type="radio" 
+            value="no"
+            onChange={this.handleChange}
+            checked={this.state.value === "no"}
+          />
+          <label for="no">No</label>
+          <button onClick={this.handleFinalSubmit}>Finish</button>
         </form>
       );
     }
   }
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+
 import Form from './form/Form';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
