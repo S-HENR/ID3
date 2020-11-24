5a3b54b0fe0d12989978dc67cfccc49b47d7ee7a
S-HENR <seraphin.henry@orange.fr>
2020-11-25 00:26:00 +0200

    Update client : - Thanks the user after his contribution
    store answered questions in order to ask others
    questions if the ID3 was inaccurate

--- client/src/form/adaptativeForm.js
+++ client/src/form/adaptativeForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { ID3 } from '../services/surveyService';
+import ThanksPage from '../thanksPage';
 import { QUESTIONS } from './questions';
 
 export default class AdaptativeForm extends React.Component {
@@ -8,11 +9,27 @@ export default class AdaptativeForm extends React.Component {
   constructor(props) {
     super(props)
     this.survey = ID3
+    this.answeredQuestions = {
+      outlook : "",
+      temp : "",
+      humidity : "",
+      wind : "",
+      friends_avail : "",
+      homework : "",
+      day_night : "",
+      localisation : "",
+      lights : "",
+      inj_sick : "",
+      transport : "",
+      result : ""
+    }
     this.buildQuestion()
   }
   survey
   currentQuestion
   isFinalQuestion = false
+  answeredQuestions
+  isSent = false
 
   state = { value:"" }
 
@@ -22,6 +39,10 @@ export default class AdaptativeForm extends React.Component {
       })
   }
 
+  addToAnsweredQuestions() {
+    this.answeredQuestions[this.currentQuestion.question] = this.state.value
+  }
+
   handleIntermediateSubmit = (e) => {
     e.preventDefault()
     if(this.state.value === "")
@@ -29,8 +50,14 @@ export default class AdaptativeForm extends React.Component {
 
     this.survey = this.survey[this.state.value]
 
+    this.addToAnsweredQuestions()
+
     if(typeof(this.survey) == "string") {
       this.isFinalQuestion = true
+      this.currentQuestion = {
+        question: 'result',
+        answers: ['yes','no']
+      }
     }
     else {
       this.buildQuestion()
@@ -43,8 +70,12 @@ export default class AdaptativeForm extends React.Component {
 
   handleFinalSubmit = (e) => {
     e.preventDefault()
+
+    this.addToAnsweredQuestions()
+
     if(this.survey === this.state.value) {
-      alert('Thank you for your time !')
+      this.isSent = true
+      this.forceUpdate()
     }
     else {
       this.askOtherQuestions()
@@ -59,7 +90,6 @@ export default class AdaptativeForm extends React.Component {
       answers: Object.keys(value)
     }
     this.survey = this.survey[key]
-
   }
 
   getQuestion(idQuestion) {
@@ -79,49 +109,86 @@ export default class AdaptativeForm extends React.Component {
         isInput = q.answers[0] === 'open'
       }
     })
-    if(!this.isFinalQuestion) {
-      if(isInput) {
-        return (
-          <form className="ml-10">
-            <div className="mb-5 text-xl text-c2 text-gray-700 py-2 px-2 border-l-4 border-yellow-300">
-              {this.getQuestion(this.currentQuestion.question)}
-            </div>
-            <div className="ml-10">
-              <input 
-                className="shadow appearance-none border rounded py-2 px-3 text-grey-darker border border-yellow-300"
-                value={this.state.value}
-                onChange={this.handleChange}
-              />
-            </div>
-            <button className="float-right bg-white text-xl text-gray-800 font-bold rounded border-b-2 border-yellow-300 shadow-md py-2 px-6 inline-flex items-center" onClick={this.handleIntermediateSubmit}>
-              <span className="mr-2">Next</span>
-              <img width="18" height="18" src="../../assets/right-arrow.svg" className="ml-4 animate-bounce my-auto"></img>
-            </button>
-          </form>
-        );
+    if(!this.isSent) {
+      if(!this.isFinalQuestion) {
+        if(isInput) {
+          return (
+            <form className="ml-10">
+              <div className="mb-5 text-xl text-c2 text-gray-700 py-2 px-2 border-l-4 border-yellow-300">
+                {this.getQuestion(this.currentQuestion.question)}
+              </div>
+              <div className="ml-10">
+                <input 
+                  className="shadow appearance-none border rounded py-2 px-3 text-grey-darker border border-yellow-300"
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <button className="float-right bg-white text-xl text-gray-800 font-bold rounded border-b-2 border-yellow-300 shadow-md py-2 px-6 inline-flex items-center" onClick={this.handleIntermediateSubmit}>
+                <span className="mr-2">Next</span>
+                <img width="18" height="18" src="../../assets/right-arrow.svg" className="ml-4 animate-bounce my-auto"></img>
+              </button>
+            </form>
+          );
+        }
+        else {
+          return (
+            <form className="ml-10">
+              <div className="mb-5 text-xl text-c2 text-gray-700 py-2 px-2 border-l-4 border-yellow-300">
+                {this.getQuestion(this.currentQuestion.question)}
+              </div>
+              <div className="ml-10">
+                {this.currentQuestion.answers.map(answer => 
+                  <div className="mb-2">
+                    <input 
+                      id={answer}
+                      type="radio" 
+                      value={answer}
+                      onChange={this.handleChange}
+                      checked={this.state.value === answer}
+                    />
+                    <label for={answer} className="ml-2 capitalize text-lg text-c2 text-gray-700">{answer}</label>
+                  </div>
+                )}
+              </div>
+              <button className="float-right bg-white text-xl text-gray-800 font-bold rounded border-b-2 border-yellow-300 shadow-md py-2 px-6 inline-flex items-center" onClick={this.handleIntermediateSubmit}>
+                <span className="mr-2">Next</span>
+                <img width="18" height="18" src="../../assets/right-arrow.svg" className="ml-4 animate-bounce my-auto"></img>
+              </button>
+            </form>
+          );
+        }
       }
       else {
         return (
-          <form className="ml-10">
-            <div className="mb-5 text-xl text-c2 text-gray-700 py-2 px-2 border-l-4 border-yellow-300">
-              {this.getQuestion(this.currentQuestion.question)}
+          <form> 
+            <div className="mb-5 text-xl text-c2 text-gray-700 py-2 px-2 border-l-4 border-yellow-300 capitalize">
+              Do you want to play ?
             </div>
             <div className="ml-10">
-              {this.currentQuestion.answers.map(answer => 
                 <div className="mb-2">
                   <input 
-                    id={answer}
+                    id="yes"
                     type="radio" 
-                    value={answer}
+                    value="yes"
                     onChange={this.handleChange}
-                    checked={this.state.value === answer}
+                    checked={this.state.value === "yes"}
                   />
-                  <label for={answer} className="ml-2 capitalize text-lg text-c2 text-gray-700">{answer}</label>
+                  <label for="yes" className="ml-2 capitalize text-lg text-c2 text-gray-700">Yes, Totally !</label>
                 </div>
-              )}
+                <div className="mb-2">
+                  <input 
+                    id="no"
+                    type="radio" 
+                    value="no"
+                    onChange={this.handleChange}
+                    checked={this.state.value === "no"}
+                  />
+                  <label for="no" className="ml-2 capitalize text-lg text-c2 text-gray-700">Nah, Maybe another time !</label>
+                </div>
             </div>
-            <button className="float-right bg-white text-xl text-gray-800 font-bold rounded border-b-2 border-yellow-300 shadow-md py-2 px-6 inline-flex items-center" onClick={this.handleIntermediateSubmit}>
-              <span className="mr-2">Next</span>
+            <button className="float-right bg-white text-xl text-gray-800 font-bold rounded border-b-2 border-yellow-300 shadow-md py-2 px-6 inline-flex items-center" onClick={this.handleFinalSubmit}>
+              <span className="mr-2">Finish</span>
               <img width="18" height="18" src="../../assets/right-arrow.svg" className="ml-4 animate-bounce my-auto"></img>
             </button>
           </form>
@@ -129,39 +196,8 @@ export default class AdaptativeForm extends React.Component {
       }
     }
     else {
-      return (
-        <form> 
-          <div className="mb-5 text-xl text-c2 text-gray-700 py-2 px-2 border-l-4 border-yellow-300 capitalize">
-            Do you want to play ?
-          </div>
-          <div className="ml-10">
-              <div className="mb-2">
-                <input 
-                  id="yes"
-                  type="radio" 
-                  value="yes"
-                  onChange={this.handleChange}
-                  checked={this.state.value === "yes"}
-                />
-                <label for="yes" className="ml-2 capitalize text-lg text-c2 text-gray-700">Yes, Totally !</label>
-              </div>
-              <div className="mb-2">
-                <input 
-                  id="no"
-                  type="radio" 
-                  value="no"
-                  onChange={this.handleChange}
-                  checked={this.state.value === "no"}
-                />
-                <label for="no" className="ml-2 capitalize text-lg text-c2 text-gray-700">Nah, Maybe another time !</label>
-              </div>
-          </div>
-          <button className="float-right bg-white text-xl text-gray-800 font-bold rounded border-b-2 border-yellow-300 shadow-md py-2 px-6 inline-flex items-center" onClick={this.handleFinalSubmit}>
-            <span className="mr-2">Finish</span>
-            <img width="18" height="18" src="../../assets/right-arrow.svg" className="ml-4 animate-bounce my-auto"></img>
-          </button>
-        </form>
-      );
+      return <ThanksPage/>
     }
+    
   }
 }
