6b00393693e7eb56ad83c0d831f486b91136a87b
S-HENR <seraphin.henry@orange.fr>
2020-11-25 00:09:49 +0200

    Update client : - Handle input element

--- client/src/form/adaptativeForm.js
+++ client/src/form/adaptativeForm.js
@@ -68,38 +68,65 @@ export default class AdaptativeForm extends React.Component {
     return question
   }
 
-  askOtherQuestions() {
+  askOthersQuestions() {
     console.log("ok let's go")
   }
 
-
   render() {
+    let isInput
+    QUESTIONS.find((q)=> {
+      if(q.id === this.currentQuestion.question) {
+        isInput = q.answers[0] === 'open'
+      }
+    })
     if(!this.isFinalQuestion) {
-      return (
-        <form className="ml-10">
-          <div className="mb-5 text-xl text-c2 text-gray-700 py-2 px-2 border-l-4 border-yellow-300">
-            {this.getQuestion(this.currentQuestion.question)}
-          </div>
-          <div className="ml-10">
-            {this.currentQuestion.answers.map(answer => 
-              <div className="mb-2">
-                <input 
-                  id={answer}
-                  type="radio" 
-                  value={answer}
-                  onChange={this.handleChange}
-                  checked={this.state.value === answer}
-                />
-                <label for={answer} className="ml-2 capitalize text-lg text-c2 text-gray-700">{answer}</label>
-              </div>
-            )}
-          </div>
-          <button className="float-right bg-white text-xl text-gray-800 font-bold rounded border-b-2 border-yellow-300 shadow-md py-2 px-6 inline-flex items-center" onClick={this.handleIntermediateSubmit}>
-            <span className="mr-2">Next</span>
-            <img width="18" height="18" src="../../assets/right-arrow.svg" className="ml-4 animate-bounce my-auto"></img>
-          </button>
-        </form>
-      );
+      if(isInput) {
+        return (
+          <form className="ml-10">
+            <div className="mb-5 text-xl text-c2 text-gray-700 py-2 px-2 border-l-4 border-yellow-300">
+              {this.getQuestion(this.currentQuestion.question)}
+            </div>
+            <div className="ml-10">
+              <input 
+                className="shadow appearance-none border rounded py-2 px-3 text-grey-darker border border-yellow-300"
+                value={this.state.value}
+                onChange={this.handleChange}
+              />
+            </div>
+            <button className="float-right bg-white text-xl text-gray-800 font-bold rounded border-b-2 border-yellow-300 shadow-md py-2 px-6 inline-flex items-center" onClick={this.handleIntermediateSubmit}>
+              <span className="mr-2">Next</span>
+              <img width="18" height="18" src="../../assets/right-arrow.svg" className="ml-4 animate-bounce my-auto"></img>
+            </button>
+          </form>
+        );
+      }
+      else {
+        return (
+          <form className="ml-10">
+            <div className="mb-5 text-xl text-c2 text-gray-700 py-2 px-2 border-l-4 border-yellow-300">
+              {this.getQuestion(this.currentQuestion.question)}
+            </div>
+            <div className="ml-10">
+              {this.currentQuestion.answers.map(answer => 
+                <div className="mb-2">
+                  <input 
+                    id={answer}
+                    type="radio" 
+                    value={answer}
+                    onChange={this.handleChange}
+                    checked={this.state.value === answer}
+                  />
+                  <label for={answer} className="ml-2 capitalize text-lg text-c2 text-gray-700">{answer}</label>
+                </div>
+              )}
+            </div>
+            <button className="float-right bg-white text-xl text-gray-800 font-bold rounded border-b-2 border-yellow-300 shadow-md py-2 px-6 inline-flex items-center" onClick={this.handleIntermediateSubmit}>
+              <span className="mr-2">Next</span>
+              <img width="18" height="18" src="../../assets/right-arrow.svg" className="ml-4 animate-bounce my-auto"></img>
+            </button>
+          </form>
+        );
+      }
     }
     else {
       return (
